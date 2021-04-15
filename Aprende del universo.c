41c065c166637ae1aff019efe5e2b6a190e3b038
Danielgomezchaves <80400890+Danielgomezchaves@users.noreply.github.com>
2021-04-15 18:51:41 +0200

    Update Aprende del universo.c

--- Aprende del universo.c
+++ Aprende del universo.c
@@ -183,8 +183,51 @@ if (opcion == 1){
 							}		                          		                    		                  	                    
 				break;
 				case 'C':
-					printf("La conquista el espacio.\n");
-					
+					printf("La conquista el espacio:\n");
+					printf("Con este titulo tan sorprendente nos referimos al comienzo  \n");
+					printf("de la exploracion espacial por parte de la especie humana.\n");
+					printf(" Escoje uno entre los siguientes temas sobre la conquista el espacio:\n");					
+					        printf("-El origen de la conquista espacial.  (C)\n");
+ 		                    printf("-Objetivo: la Luna.  (O)\n");
+ 		                    printf("-La estacion espacial internacional.  (E)\n");
+ 		                    printf("-Un proyecto de base lunar.  (B)\n");
+ 		                    printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		                    fflush(stdin);
+ 		                    scanf("%c", &temaSecundario1);
+							switch (temaSecundario1){
+						
+					        	case 'C':
+					                printf("El origen de la conquista espacial:\n");
+					                printf("La conquista del espacio comenzo realmente con el lanzamiento\n");
+					                printf("del primer satelite, el  Sputnik 1, el 4 de octubre de 1957.\n");
+					                printf("Fueron los sovieticos quienes consiguieron tal\n");
+					                printf(" proeza, ahi el nombre tan raro del satelite.\n");					
+				                break;
+				                case 'O':
+					                printf("Objetivo: la Luna:\n");
+					                printf("El 16 de julio de 1969, una tripulacion formada por tres austronautas\n");
+					                printf("americanos vivio en sus propias carnes la mayor aventura de la humanidad\n");
+					                printf("hasta la fecha: despegaron hacia la Luna a bordo del cohete Saturno V.\n");
+					                printf("Esta mision llamada Apolo 11, fue la primera en mandar humanos al espacio,\n");
+					                printf("los intrepidos astronautas fueron: Neil Armstrong, Michael Collins y Buzz Aldrin.\n");					
+				                break;
+				                case 'E':
+				 	                printf("La estacion espacial internacional:\n");
+				 	                printf("Esta situada a 400 km de altura sobre la Tierra, es la mayor nabe espacial jamas\n");
+				 	                printf("construida, 16 paises colaboraron en su construccion. La funcion de esta estructura\n");
+				 	                printf("gigantesca es la de realizar experimentos cientificos y observar el universo,\n");
+				 	                printf("da una vuelta a la Tierra cada 90 minutos. Mide 73 metros de ancho y 109 metros de largo.\n");				 	                
+				                break;
+				                case 'B':
+				 	                printf("Un proyecto de base lunar:\n");
+				 	                printf("Esta previsto como proyecto establecer una base en la Luna. Para que las tripulaciones con\n");
+				 	                printf("destino a Marte hicieran escala alli, entre otras funciones. La base incluiria modulos de vivienda y \n");
+				 	                printf("modulos de laboratorio, paneles solares para proporcionar la energia necesaria para disponer de luz y \n");
+							printf("calefaccion. Alli se instalarian hasta una docena de cientificos para investigar la Luna y sus misterios.\n");				
+				                break;				                
+				                default:
+				                    printf("\n");
+							}	
 				break;
 			default:
 				printf("\n");
