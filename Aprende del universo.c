a4344d50c76005ab650ebb858f42969f8331fa86
IreneJimenezCasas <80333484+IreneJimenezCasas@users.noreply.github.com>
2021-04-21 12:38:02 +0200

    Update Aprende del universo.c

--- Aprende del universo.c
+++ Aprende del universo.c
@@ -198,6 +198,221 @@ if (opcion == 1){
 						
 					        	case 'C':
 					                printf("El origen de la conquista espacial:\n");
+#include<stdio.h>
+#define N 100
+#define S 50
+
+struct jugador{
+	char nombre[S];
+	int puntuacion;
+};
+int main() {
+	int  x;
+	char temaPrincipal, temaSecundario, temaSecundario1 ;
+	struct jugador lista[N];
+	int opcion, n=1,i=0;
+	char curiosidad;
+	
+        printf("\n		## Bienvenido a Aprender sobre el Sistema Solar ##\n\n");
+do{
+ 	printf("¿Te sientes preparado para evaluar tus conocimientos o quieres aprender mas sobre el espacio?\n");
+	 printf("Necesitas saber mas sobre el universo (1).\n");
+ 	printf("Te consideras preparado/a (2).\n");
+ 	scanf("%d",&opcion);
+ 
+ 	
+if (opcion == 1){
+ 		printf("- El Universo (U)\n");
+ 		printf("- El Sistema solar (S)\n");
+ 		printf("- La Tierra (T)\n");
+ 		printf("- La Luna (L)\n");
+ 		printf("- La conquista el espacio (C)\n");
+ 		printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		fflush(stdin);
+			scanf("%c", &temaPrincipal);
+			switch (temaPrincipal) {
+				case 'U':
+					printf("El Universo:\n\n");
+					printf("El Universo se creo hace 14.000 millones de anyos tras una explosion gigante llamada Big Bang, que significa <la gran explosion>.\n");
+					printf("\nEl universo es enorme, hemos seleccionado los temas mas interesantes.\n");
+					printf(" Escoje uno entre los siguientes:\n");					
+					printf("-Las Galaxias.  (G)\n");
+ 		            printf("-El nacimiento del Sol.  (S)\n");
+ 		            printf("-La formacion de los planetas.  (P)\n");
+ 		            printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		            fflush(stdin);
+ 		            scanf("%c", &temaSecundario);
+					switch (temaSecundario){
+				        case 'G':
+					        printf("Las Galaxias:\n\n");
+							printf("Una galaxia es el conjunto de estrellas, planetas, cometas, nubes de gas y de polvo que giran alrededor de un mismo nucleo.\n");
+							printf("Querrias saber un dato curioso...\n");
+							printf("Si quiere conocerlo pulse (S) y si quiere continuar pulse (N)\n");
+							fflush(stdin);
+							scanf("%c", &curiosidad);
+							if((curiosidad == 'S') || (curiosidad == 's')) {
+								printf("A veces una galaxia masiva atrae a una galaxia mas  pequeña. Poco a poco las estrellas y las nubes de gas van a girar alrededor de un mismo nucleo.\n\n");
+							} else if ((curiosidad == 'N') || (curiosidad == 'n')) {
+								printf("Pues tu te lo pierdes :)\n\n");
+							} else {
+								printf("Error, opcion incorrecta\n\n");
+							}
+				        break;
+				        case 'S':
+					        printf("El nacimiento del Sol:\n");
+					
+				        break;
+				        case 'P':
+				 	        printf("La formacion de los planetas:\n");
+					
+				        break;
+			            default:
+				            printf("\n");
+			        }    
+				break;
+				case 'S':
+					printf("El Sistema Solar.\n");
+					printf("Definicion .\n");
+					printf("El Sistema solar es muy grande, hemos seleccionado los temas mas interesantes,\n");
+					printf(" Escoje uno entre los siguientes:\n");					
+					printf("-Observando el cielo.  (C)\n");
+ 		            printf("-Observando las estrellas.  (E)\n");
+ 		            printf("-Nuestra estrella el Sol.  (S)\n");
+ 		            printf("-Los planetas.  (P)\n");
+ 		            printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		            fflush(stdin);
+ 		            scanf("%c", &temaSecundario);
+					switch (temaSecundario){
+				        case 'C':
+					        printf("Observando el cielo:\n");
+					
+				        break;
+				        case 'E':
+					        printf("Observando las estrellas:\n");
+					
+				        break;
+				        case 'S':
+				 	        printf("Nuestra estrella el Sol:\n");
+					
+				        break;
+				        case 'P':
+				 	        printf("Los planetas y demas cuerpos estelares:\n");
+					        printf(" Escoje uno entre los siguientes:\n");					
+					        printf("-Mercurio.  (M)\n");
+ 		                    printf("-Venus.  (V)\n");
+ 		                    printf("-La Tierra.  (T)\n");
+ 		                    printf("-Marte.  (R)\n");
+ 		                    printf("-Jupiter.  (J)\n");
+ 		                    printf("-Saturno.  (S)\n");
+ 		                    printf("-Urano y neptuno.  (U)\n");
+ 		                    printf("-Asteroides.  (A)\n");
+ 		                    printf("-Metioritos. (E)\n");
+ 		                    printf("-Cometas.  (C)\n");
+ 		                    printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		                    fflush(stdin);
+ 		                    scanf("%c", &temaSecundario1);
+					        switch (temaSecundario1){
+					        	case 'M':
+					                printf("Mercurio:\n");
+					
+				                break;
+				                case 'V':
+					                printf("Venus:\n");
+					
+				                break;
+				                case 'T':
+				 	                printf("La Tierra:\n");
+					
+				                break;
+				                case 'R':
+				 	                printf("Marte:\n");
+					
+				                break;
+				                case 'J':
+				 	                printf("Jupiter:\n");
+					
+				                break;
+				                case 'S':
+				 	                printf("Saturno:\n");
+					
+				                break;
+				                case 'U':
+				 	                printf("Urano y Neptuno:\n");
+					
+				                break;
+				                case 'A':
+				 	                printf("Asteroides:\n");
+					
+				                break;
+				                case 'E':
+				 	                printf("Metioritos:\n");
+					
+				                break;
+				                case 'C':
+				 	                printf("Cometas:\n");
+					
+				                break;
+				                default:
+				                    printf("\n");
+							}
+				        break;
+			            default:
+				            printf("\n");
+			        }     
+				break;
+				case 'T':
+					printf("La Tierra.\n");
+					
+				break;
+				case 'L':
+					printf("La Luna.\n");
+					printf("Definicion.\n");
+					    printf(" Escoje uno entre los siguientes temas sobre la Luna:\n");					
+					        printf("-La Luna satelite de la Tierra.  (L)\n");
+ 		                    printf("-Superficie.  (S)\n");
+ 		                    printf("-Caras de la Luna.  (C)\n");
+ 		                    printf("-Curiosidades.  (U)\n");
+ 		                    printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		                    fflush(stdin);
+ 		                    scanf("%c", &temaSecundario1);
+							switch (temaSecundario1){
+						
+					        	case 'L':
+					                printf("La Luna satelite de la Tierra:\n");
+					
+				                break;
+				                case 'S':
+					                printf("Superficie:\n");
+					
+				                break;
+				                case 'C':
+				 	                printf("Caras de la Luna:\n");
+					
+				                break;
+				                case 'U':
+				 	                printf("Curiosidades:\n");
+					
+				                break;				                
+				                default:
+				                    printf("\n");
+							}		                          		                    		                  	                    
+				break;
+				case 'C':
+					printf("La conquista el espacio:\n");
+					printf("Con este titulo tan sorprendente nos referimos al comienzo  \n");
+					printf("de la exploracion espacial por parte de la especie humana.\n");
+					printf(" Escoje uno entre los siguientes temas sobre la conquista el espacio:\n");					
+					        printf("-El origen de la conquista espacial.  (C)\n");
+ 		                    printf("-Objetivo: la Luna.  (O)\n");
+ 		                    printf("-La estacion espacial internacional.  (E)\n");
+ 		                    printf("-Un proyecto de base lunar.  (B)\n");
+ 		                    printf("(RECORDATORIO: Introduce el caracter en mayuscula)\n");
+ 		                    fflush(stdin);
+ 		                    scanf("%c", &temaSecundario1);
+							switch (temaSecundario1){
+						
+					        	case 'C':
+					                printf("El origen de la conquista espacial:\n");
 					                printf("La conquista del espacio comenzo realmente con el lanzamiento\n");
 					                printf("del primer satelite, el  Sputnik 1, el 4 de octubre de 1957.\n");
 					                printf("Fueron los sovieticos quienes consiguieron tal\n");
@@ -238,7 +453,7 @@ else if (opcion = 2) {
 	printf("Registrate:\n");
 	
 }
-printf("¿Quieres continuar? Pulse 1 si quiere volver al menu de inicio. O 2 si quiere cerrar el programa.\n");
+printf("¿Quieres continuar? Pulse 1 si quiere volver al menu de inicio o 2 si quiere cerrar el programa.\n");
 scanf("%d", &x);
 }
 while(x==1);
